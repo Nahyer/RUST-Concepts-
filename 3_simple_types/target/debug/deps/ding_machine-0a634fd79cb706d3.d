e139463c49b8cfcc24dae0e18f156715e4897351
Nahyer <105147076+Nahyer@users.noreply.github.com>
2023-02-18 11:07:47 +0300

    Delete 3_simple_types/target directory

--- 3_simple_types/target/debug/deps/ding_machine-0a634fd79cb706d3.d
+++
@@ -1,7 +0,0 @@
-C:\Users\REYHAN\RUST\exercises\ultimate_rust_crash_course\exercise\3_simple_types\target\debug\deps\ding_machine-0a634fd79cb706d3.rmeta: src\lib.rs
-
-C:\Users\REYHAN\RUST\exercises\ultimate_rust_crash_course\exercise\3_simple_types\target\debug\deps\libding_machine-0a634fd79cb706d3.rlib: src\lib.rs
-
-C:\Users\REYHAN\RUST\exercises\ultimate_rust_crash_course\exercise\3_simple_types\target\debug\deps\ding_machine-0a634fd79cb706d3.d: src\lib.rs
-
-src\lib.rs:
--- 3_simple_types/target/debug/deps/ding_machine-2648fd944dd3055b.d
+++
@@ -1,5 +0,0 @@
-C:\Users\REYHAN\RUST\exercises\ultimate_rust_crash_course\exercise\3_simple_types\target\debug\deps\ding_machine-2648fd944dd3055b.rmeta: src\lib.rs
-
-C:\Users\REYHAN\RUST\exercises\ultimate_rust_crash_course\exercise\3_simple_types\target\debug\deps\ding_machine-2648fd944dd3055b.d: src\lib.rs
-
-src\lib.rs:
