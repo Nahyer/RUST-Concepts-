4baed2bcc830da21465d2eafe384c53549e2226d
Nahyer <105147076+Nahyer@users.noreply.github.com>
2023-02-18 11:12:04 +0300

    Delete 2_functions/target directory

--- 2_functions/target/debug/deps/functions-b58201d1474669f4.d
+++
@@ -1,5 +0,0 @@
-C:\Users\REYHAN\RUST\exercises\ultimate_rust_crash_course\functions\target\debug\deps\functions-b58201d1474669f4.rmeta: src\main.rs
-
-C:\Users\REYHAN\RUST\exercises\ultimate_rust_crash_course\functions\target\debug\deps\functions-b58201d1474669f4.d: src\main.rs
-
-src\main.rs:
